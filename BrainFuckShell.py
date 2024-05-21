8dfdd8b2c094959d6b1daba3e3c9f7e3f7c87362
localhost <87206252+PySudo@users.noreply.github.com>
2024-05-21 23:39:21 +0330

    Add files via upload

---
+++ BrainFuckShell.py
@@ -0,0 +1,16 @@
+from brainfuck import CompileBF
+MyCompiler = CompileBF(StackAutoClear=False)
+print('Enter your BF codes.\nFor get stack just type "stack"')
+while True:
+    try:
+        code = input('---> : ')
+        if code == 'stack':
+            print(MyCompiler.Stack)
+        elif all(i in '[+-].,<>' for i in code):
+            output = MyCompiler.ExectueCode(code)
+            if output:
+                print('output is :',output)
+            print('pointer :', MyCompiler.Pointer)
+        print('-'*15)
+    except KeyboardInterrupt:
+        break
