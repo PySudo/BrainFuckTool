8293dee8389866a8bbd9c86203189cd8debeecca
omid <omid377773@gmail.com>
2024-05-23 20:58:54 +0330

    add comments

--- DebuggerExample.py
+++ DebuggerExample.py
@@ -1,7 +1,15 @@
 from brainfuck import Debug
+
+# Your BrainFuck Code :
 MyCode = '->+++++++[<++++++++++++++>-]+++++[>+++++<-]>[<<.+>>-]<<.'
-MyDebugger = Debug(StackAutoClear=False)
+
+# Create a instance :
+MyDebugger = Debug(StackAutoClear=False) # Also you can get code like "Debug(Mycode, StackAutoClear=False)"
+
+# Set Brainfuck Code :
 MyDebugger.Code = MyCode
+
+# Start Debugging :
 print(('-'*5)+' Start Debugging '+('-'*5))
 for i in MyDebugger:
     print(i)
